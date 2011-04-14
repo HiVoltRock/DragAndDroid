cfb801464e9327e34b57b106792a0655c6219651
Cory M <HiVoltList@gmail.com>
2011-04-14 10:03:35 -0400

    Added test Android project

---
+++ TestBed/src/com/DragAndDroid/TestBed/TestBed.java
@@ -0,0 +1,13 @@
+package com.DragAndDroid.TestBed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestBed extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
