9b3aeec699cd7eab18735ba707292c46a29b17bd
afavia <afavia.student@manhattan.edu>
2011-04-28 11:43:33 -0400

    cleaned project

--- TestBed/src/com/DragAndDroid/TestBed/TestBed.java
+++ TestBed/src/com/DragAndDroid/TestBed/TestBed.java
@@ -2,9 +2,6 @@ package com.DragAndDroid.TestBed;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class TestBed extends Activity {
     /** Called when the activity is first created. */
@@ -13,13 +10,4 @@ public class TestBed extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
-
-
-	public void btn1_onClick(View view)
-	{
-		TextView tv = (TextView) findViewById(R.id.label_);
-		EditText et = (EditText) findViewById(R.id.txtBox);
-		
-		tv.setText( et.getText().toString());
-	}
 }
